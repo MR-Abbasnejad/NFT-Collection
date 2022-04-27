2f45e0b4b3fdfdd77767be2ec086724b0dd729fd
MR-Abbasnejad <m.abbasnezhad.80@gmail.com>
2022-04-27 15:33:14 +0430

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x43aC5fCd21e7f5cB08C4075fDC2a197796Db4DA9";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://my-whitelist-dapp.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
